6cfbfa609cefd1528a11f0268367cc9bb9bbdfdb
unknown <leonardo.filipe@yahoo.com>
2024-12-19 22:35:46 -0300

    Adicionando container com slid

--- src/app/components/container-main/container-main.component.ts
+++ src/app/components/container-main/container-main.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrl: './container-main.component.css'
 })
 export class ContainerMainComponent {
+  titles = ['Manchete numero 1', 'Manchete numero 2', 'Manchete numero 3'];
+  descriptions = [
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    'Dolor Sit Amet has been the industry\'s standard dummy text.',
+    'Consectetur Adipiscing is used by designers to focus on layout and content.'
+  ];
+  currentIndex = 0;
 
+  navigate(direction: 'left' | 'right'): void {
+    if (direction === 'left' && this.currentIndex > 0) {
+      this.currentIndex--;
+    } else if (direction === 'right' && this.currentIndex < this.titles.length - 1) {
+      this.currentIndex++;
+    }
+  }
 }
