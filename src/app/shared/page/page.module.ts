5f3781ef100013f28ac677b2c7afb45bcd3e0e18
unknown <leonardo.filipe@yahoo.com>
2024-12-22 20:38:52 -0300

    adicionando outras telas

--- src/app/shared/page/page.module.ts
+++ src/app/shared/page/page.module.ts
@@ -14,7 +14,7 @@ import { FooterComponent } from '../../components/footer/footer.component';
     ContainerMainComponent,
     ContainerSmallComponent,
     ContainerMapComponent,
-    FooterComponent
+    FooterComponent,
   ],
   imports: [
     CommonModule,
